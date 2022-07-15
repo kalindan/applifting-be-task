5a1804384096fe2fd65266c07617dee6d75f50dc
Kalindan <dikey@email.cz>
2022-07-15 22:14:51 +0200

    refactor

--- app/db/crud.py
+++ app/db/crud.py
@@ -13,19 +13,19 @@ class CRUDProduct:
         return product
 
     @staticmethod
-    def read_by_name(name: str, session: Session) -> Product:
+    def read_by_name(name: str, session: Session) -> Product | None:
         product = session.exec(
             select(Product).where(Product.name == name)
         ).first()
         if not product:
-            raise HTTPException(status_code=404, detail="Product not found")
+            return None
         return product
 
     @staticmethod
-    def read_by_id(id: int, session: Session) -> Product:
+    def read_by_id(id: int, session: Session) -> Product | None:
         product = session.get(Product, id)
         if not product:
-            raise HTTPException(status_code=404, detail="Product not found")
+            return None
         return product
 
     @staticmethod
@@ -33,51 +33,36 @@ class CRUDProduct:
         return session.exec(select(Product)).all()
 
     @staticmethod
-    def delete_by_name(name: str, session: Session) -> None:
-        product = session.exec(
-            select(Product).where(Product.name == name)
-        ).first()
-        if not product:
-            raise HTTPException(status_code=404, detail="Product not found")
-        session.delete(product)
-        session.commit()
-        return
-
-    @staticmethod
-    def delete_by_id(id: int, session: Session) -> None:
-        product = session.get(Product, id)
-        if not product:
-            raise HTTPException(status_code=404, detail="Product not found")
-        session.delete(product)
-        session.commit()
-        return
-
-    @staticmethod
-    def is_in_db(name: str, session: Session) -> bool:
-        product = session.exec(
-            select(Product).where(Product.name == name)
-        ).first()
-        if not product:
-            return False
-        return True
-
-    @staticmethod
-    def update_description_by_id(
-        id: int, description: str, session: Session
-    ) -> Product:
-        product = session.get(Product, id)
-        if not product:
-            raise HTTPException(status_code=404, detail="Product not found")
-        product.description = description
+    def update(product: Product, session: Session) -> Product:
         session.add(product)
         session.commit()
         session.refresh(product)
         return product
 
+    @staticmethod
+    def delete(product: Product, session: Session) -> None:
+        session.delete(product)
+        session.commit()
+        return
+
 
 class CRUDOffer:
     @staticmethod
     def create(offer: Offer, session: Session) -> Offer:
+        session.add(offer)
+        session.commit()
+        session.refresh(offer)
+        return offer
+
+    @staticmethod
+    def read_by_id(id: int, session: Session) -> Offer | None:
+        offer = session.get(Offer, id)
+        if not offer:
+            return None
+        return offer
+
+    @staticmethod
+    def update(offer: Offer, session: Session) -> Offer:
         session.add(offer)
         session.commit()
         session.refresh(offer)
