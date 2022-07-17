59a033624ee67b83fe81990703cbc34f1ab533b0
Kalindan <dikey@email.cz>
2022-07-18 00:18:09 +0200

    change DB_URL to DATABASE_URL

--- app/config/config.py
+++ app/config/config.py
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
     offer_token: str | None = os.environ.get("OFFER_TOKEN")
     offer_url: str | None = os.environ.get("OFFER_URL")
     secret_key: str = os.environ.get("SECRET_KEY", "123456789")
-    db_url: str = os.environ.get("DB_URL", "sqlite:///database.db")
+    db_url: str = os.environ.get("DATABASE_URL", "sqlite:///database.db")
     offer_refresh_rate_sec: int = Field(
-        default=os.environ.get("OFFER_REFRESH_RATE_SEC", 300)
+        default=os.environ.get("OFFER_REFRESH_RATE_SEC", 60)
     )
     jwt_expiration_time_min: int = Field(
         default=os.environ.get("JWT_EXPIRATION_TIME_MIN", 5)
