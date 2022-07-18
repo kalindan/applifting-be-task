638ef5d9e249394f657051e08a77987f493b3691
Kalindan <dikey@email.cz>
2022-07-18 02:04:59 +0200

    add  default offer token and url

--- app/config/config.py
+++ app/config/config.py
@@ -6,8 +6,13 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     admin_username: str = os.environ.get("ADMIN_USERNAME", "admin")
     admin_password: str = os.environ.get("ADMIN_PASSWORD", "admin")
-    offer_token: str | None = os.environ.get("OFFER_TOKEN")
-    offer_url: str | None = os.environ.get("OFFER_URL")
+    offer_token: str | None = os.environ.get(
+        "OFFER_TOKEN", "c1287585-083c-4250-99dd-72d9f413106d"
+    )
+    offer_url: str | None = os.environ.get(
+        "OFFER_URL",
+        "https://applifting-python-excercise-ms.herokuapp.com/api/v1",
+    )
     secret_key: str = os.environ.get("SECRET_KEY", "123456789")
     db_url: str = os.environ.get("DATABASE_URL", "sqlite:///database.db")
     offer_refresh_rate_sec: int = Field(
